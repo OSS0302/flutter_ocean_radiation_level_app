70167dc3dd4a4871f6d7f255910335cd6ec773db
ohsangsoo <tkdtn3264@gmail.com>
2023-10-04 19:18:26 +0900

    홈 컨트롤러 추가하기

---
+++ lib/presentation/pags/home/home_screen_controller.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ocean_radiation_level/data/source/get_one_data_from_api.dart';
+import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radition_level_entity.dart';
+import 'package:flutter_ocean_radiation_level/domain/model/ocean_radiation_level_list.dart';
+import 'package:flutter_ocean_radiation_level/presentation/common/string_info_for_nuclear.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class HomeScreenController extends ChangeNotifier{
+  final PanelController panelController = PanelController();
+  String panelBodyText = nuclearInfo;
+
+
+
+  bool _isLoading = true;
+  bool get isLoading => _isLoading;
+
+  final List<Map<String, dynamic>> data = [
+    {
+      'title': nuclearInfoTitle,
+      'backgroundColor': Colors.grey,
+    },
+    {
+      'title': nuclearAffectTitle,
+      'backgroundColor': Colors.red,
+    },
+    {
+      'title': nuclearChartTitle,
+      'backgroundColor': Colors.yellow,
+    },
+  ];
+
+  /// TODO: 리스트 최근 첫번째 데이터 받아와서 저장후 뿌려주기
+  Future<void> initHome() async {
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> openPanel({required String title}) async {
+    if(title.contains('영향')){
+      panelBodyText = nuclearAffectT;
+      notifyListeners();
+    } else if(title.contains('수치')){
+      panelBodyText = nuclearChart;
+      notifyListeners();
+    } else {
+      panelBodyText = nuclearInfo;
+      notifyListeners();
+    }
+    if(panelController.isPanelClosed){
+      // pannel 을 해당되는 사이즈로 오픈
+      panelController.animatePanelToPosition(0.5);
+      // // pannel max 로 오픈
+      // panelController.open();
+    }
+  }
+
+}
