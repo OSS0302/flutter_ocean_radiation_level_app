f4b7e748cad73f30479426d27f32b52946a5585b
ohsangsoo <tkdtn3264@gmail.com>
2023-10-12 22:28:13 +0900

    한달치 데이터 가져오는 로직 추가하기

--- lib/presentation/pags/history/history_screen_controller.dart
+++ lib/presentation/pags/history/history_screen_controller.dart
@@ -1,80 +1,69 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ocean_radiation_level/data/source/get_one_data_from_api.dart';
 import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radiation_level_dao.dart';
-
+import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radition_level_entity.dart';
 import 'package:flutter_ocean_radiation_level/presentation/common/string_info_for_nuclear.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:intl/intl.dart';
 
 class historyScreenController extends ChangeNotifier{
   final PanelController panelController = PanelController();
   String panelBodyText = nuclearInfo;
+  final TextEditingController searchController = TextEditingController();
   final OceanRaditionLevelDao dao = OceanRaditionLevelDao();
-
-
-
+  List<OceanRaditionLevelEntity> dataApi = [];
+  List<OceanRaditionLevelEntity> dataApiFilter = [];
+  OceanRaditionLevelEntity? selectedData;
 
   bool _isLoading = true;
 
   bool get isLoading => _isLoading;
 
-  final List<Map<String, dynamic>> data = [
-    {
-      'title': 'aaa',
-      'backgroundColor': Colors.grey,
-    },
-    {
-      'title': 'bbb',
-      'backgroundColor': Colors.red,
-    },
-    {
-      'title': 'ccc',
-      'backgroundColor': Colors.yellow,
-    },
-    {
-      'title': 'ddddd',
-      'backgroundColor': Colors.cyanAccent,
-    },
-    {
-      'title': 'eee',
-      'backgroundColor': Colors.blue,
-    },
-    {
-      'title': 'fff',
-      'backgroundColor': Colors.cyan,
-    },
-    {
-      'title': 'ggg',
-      'backgroundColor': Colors.yellow,
-    },
-
-  ];
-
   /// TODO: 리스트 최근 첫번째 데이터 받아와서 저장후 뿌려주기
   Future<void> initHistory() async {
+    await getOneDataFromApi(
+        startDate: DateFormat('yyyyMMdd').format(DateTime(DateTime.now().year, DateTime.now().month - 1, DateTime.now().day)),
+        endDate: DateFormat('yyyyMMdd').format(DateTime.now()))
+        .then((value) {
+      if (value.isNotEmpty) {
+        dataApi.addAll(value);
+        dataApiFilter.addAll(value);
+        selectedData = value[0];
+      }
+    });
 
-    dao.searchRaditionLevel('오징어');
     _isLoading = false;
-
     notifyListeners();
   }
 
-  Future<void> openPanel({required String title}) async {
-    if(title.contains('영향')){
-      panelBodyText = nuclearAffectT;
-      notifyListeners();
-    } else if(title.contains('수치')){
-      panelBodyText = nuclearChart;
-      notifyListeners();
-    } else {
-      panelBodyText = nuclearInfo;
-      notifyListeners();
-    }
+  Future<void> openPanel({required OceanRaditionLevelEntity data }) async {
+    selectedData = data;
+    notifyListeners();
+
     if(panelController.isPanelClosed){
       // pannel 을 해당되는 사이즈로 오픈
       panelController.animatePanelToPosition(0.5);
       // // pannel max 로 오픈
-      // panelController.open();
+      //panelController.open();
     }
   }
 
+  Future<void> filteredDate(String? searchVal) async {
+    if(searchVal != null && searchVal != ''){
+      dataApiFilter = [];
+      notifyListeners();
+      dataApi.forEach((element) {
+        print(element.itmNm);
+        if(element.itmNm.contains(searchVal)){
+          dataApiFilter.add(element);
+          notifyListeners();
+        }
+      });
+    } else {
+      dataApiFilter = [];
+      dataApiFilter.addAll(dataApi);
+      notifyListeners();
+    }
+  }
 }
