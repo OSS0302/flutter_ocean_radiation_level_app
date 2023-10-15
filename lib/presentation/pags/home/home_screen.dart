9982f67ce6a34162add26612fbee85ef7b195110
ohsangsoo <tkdtn3264@gmail.com>
2023-10-15 20:25:36 +0900

    다크모드 해보니 애니메이션 리스트 컬러  변경

--- lib/presentation/pags/home/home_screen.dart
+++ lib/presentation/pags/home/home_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ocean_radiation_level/data/source/get_one_data_from_api.dart';
-import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radiation_level_dao.dart';
-import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radition_level_entity.dart';
-import 'package:flutter_ocean_radiation_level/data/source/ocean_radiation_level_api.dart';
-import 'package:flutter_ocean_radiation_level/domain/model/ocean_radiation_level_list.dart';
-import 'package:flutter_ocean_radiation_level/domain/model/ocean_radiation_level_list.dart';
 import 'package:flutter_ocean_radiation_level/presentation/pags/home/home_screen_controller.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:animation_list/animation_list.dart';
@@ -38,9 +33,8 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context  ) {
     final homeViewModel = context.watch<HomeScreenController>();
-
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -48,9 +42,9 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text(
           '홈화면',
           style: TextStyle(
-              fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black),
+              fontSize: 40, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.white,
+        // backgroundColor: Colors.white,
       ),
       body: homeViewModel.isLoading
           ? const Center(
@@ -58,6 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           : SlidingUpPanel(
               controller: homeViewModel.panelController,
+              color: Theme.of(context).colorScheme.background,
               maxHeight: MediaQuery.of(context).size.height * 0.9,
               minHeight: 0,
               // minHeight: MediaQuery.of(context).size.height * 0.3,
@@ -67,60 +62,74 @@ class _HomeScreenState extends State<HomeScreen> {
               backdropEnabled: true,
               panel: _floatingPanel(),
               // collapsed: _floatingCollapsed(),
-              body: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            newestDateTile(title: '시료수거지원명: ', data: homeViewModel.dataApi[0].gathMchnNm),
-                            newestDateTile(title: '수거날짜: ', data: homeViewModel.dataApi[0].gathDt),
-                            newestDateTile(title: '품목명: ', data: homeViewModel.dataApi[0].itmNm),
-                            newestDateTile(title: '원산지: ', data: homeViewModel.dataApi[0].ogLoc),
-                            newestDateTile(title: '분석의뢰일자: ', data: homeViewModel.dataApi[0].analRqstDt),
-                            newestDateTile(title: '분석시작일자: ', data: homeViewModel.dataApi[0].analStDt),
-                            newestDateTile(title: '분석종료일자: ', data: homeViewModel.dataApi[0].analEndDt),
+              body: Container(
+                color: Theme.of(context).colorScheme.background,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.background,
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
                           ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              newestDateTile(title: '시료수거지원명: ', data: homeViewModel.dataApi[0].gathMchnNm),
+                              newestDateTile(title: '채취일자 : ', data: homeViewModel.dataApi[0].gathDt),
+                              newestDateTile(title: '품목명: ', data: homeViewModel.dataApi[0].itmNm),
+                              newestDateTile(title: '원산지: ', data: homeViewModel.dataApi[0].ogLoc),
+                              newestDateTile(title: '분석의뢰일자: ', data: homeViewModel.dataApi[0].analRqstDt),
+                              newestDateTile(title: '분석시작일자: ', data: homeViewModel.dataApi[0].analStDt),
+                              newestDateTile(title: '분석종료일자: ', data: homeViewModel.dataApi[0].analEndDt),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: AnimationList(
-                          duration: 2000,
-                          reBounceDepth: 30,
-                          children: homeViewModel.data.map((item) {
-                            return _buildTile(
-                                item['title'], item['backgroundColor']);
-                          }).toList()),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+                      child: Text(
+                          'data가 없는날도 있어서 한달치를 가져오도록 했습니다.',
+                          style: TextStyle(
+                            color: Colors.grey,
+                              fontSize: 16,
+                          ),
+                      ),
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: Center(
+                        child: AnimationList(
+                          physics: const NeverScrollableScrollPhysics(),
+                            duration: 2000,
+                            reBounceDepth: 30,
+                            children: homeViewModel.data.map((item) {
+                              return _buildTile(
+                                  item['title']);
+                            }).toList()),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
       backgroundColor: Colors.white,
     );
   }
 
-  Widget _buildTile(String? title, Color? backgroundColor) {
+  Widget _buildTile(String? title,{ Color?backgroundColor = Colors.blueAccent} ) {
     final homeViewModel = context.watch<HomeScreenController>();
     return InkWell(
       onTap: () {
@@ -140,7 +149,11 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 12.0),
             child: Text(
               title ?? '',
-              style: TextStyle(fontSize: 24),
+              style: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  // color: context.isDarkMode ? Colors.red : Colors.blue,
+              ),
             ),
           ),
         ),
@@ -150,9 +163,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _floatingPanel() {
     final homeViewModel = context.watch<HomeScreenController>();
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
+    return Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -169,15 +180,28 @@ class _HomeScreenState extends State<HomeScreen> {
                   borderRadius: BorderRadius.circular(50),
                 )),
           ),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              homeViewModel.panelBodyText,
-              style: TextStyle(fontSize: 16),
+          Flexible(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      homeViewModel.panelBodyText,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 200,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
+
         ],
-      ),
+
     );
   }
 
@@ -192,12 +216,13 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         Expanded(
-          flex: 2,
+          flex: 1,
           child: Text(
               data,
             style: const TextStyle(fontSize: 16),
           ),
         )
+
       ],
     );
   }
