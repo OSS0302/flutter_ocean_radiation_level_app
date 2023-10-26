b99bf41633996d0817778fb2dca35e920a305563
ohsangsoo <tkdtn3264@gmail.com>
2023-10-26 21:59:39 +0900

    하이브 삭제 추가하기

--- lib/data/source/local/ocean_radiation_level_dao.dart
+++ lib/data/source/local/ocean_radiation_level_dao.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_ocean_radiation_level/data/keys.dart';
 import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radition_level_entity.dart';
+import 'package:flutter_ocean_radiation_level/domain/model/ocean_radiation_level_list.dart';
 import 'package:hive/hive.dart';
 
 class OceanRaditionLevelDao {
   // 데이터 추가
   Future<void> insertraditionLevel(
-      OceanRaditionLevelEntity caditionListingEntities) async {
+      OceanRaditionLevelEntity ocaditionListingEntities) async {
     final box = await Hive.openBox<OceanRaditionLevelEntity>(db);
-    await box.add(caditionListingEntities);
+    await box.add(ocaditionListingEntities);
   }
 
   // 검색
@@ -23,14 +24,30 @@ class OceanRaditionLevelDao {
   }
 
   // 검색
-  Future<List<OceanRaditionLevelEntity>> getAllRaditionLevel(OceanRaditionLevelEntity oceanRaditionLevelEntity) async {
+  Future<List<OceanRaditionLevelEntity>> getAllRaditionLevel() async {
     final box = await Hive.openBox<OceanRaditionLevelEntity>(db);
     final List<OceanRaditionLevelEntity> OceanRadiationList = box.values.toList();
-    // print(box.keys);
+     print(box.keys);
     return OceanRadiationList;
   }
 
 
+
+// 삭제
+  Future<void> deleteData(OceanRaditionLevelEntity oceanRaditionLevelIndex) async {
+    final box = await Hive.openBox<OceanRaditionLevelEntity>(db);
+    int? key = box.values
+        .firstWhere((element) =>
+    element.smpNo == oceanRaditionLevelIndex.smpNo &&
+        element.smpNo.toString() == oceanRaditionLevelIndex.smpNo.toString())
+        .key;
+
+    if (key != null) {
+      box.delete(key);
+    } else {
+      box.delete(oceanRaditionLevelIndex);
+    }
+  }
 
   // 클리어
   Future<void> clearRaditionLevel() async {
@@ -41,27 +58,5 @@ class OceanRaditionLevelDao {
 }
 
 
-// import 'package:flutter_ocean_radiation_level/data/source/local/ocean_radition_level_entity.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-//
-// class OceanRaditionLevelDao {
-//   final openbox = Hive.box('db');
-//
-//   // 추가 캐쉬를 위해서사용한다.
-//   Future<void> insertOceanRaditionLevel(
-//       List<OceanRaditionLevelEntity> OceanRaditionLevelListEntity)async{
-//         await openbox.put('OceanRaditionLevelList',OceanRaditionLevelListEntity);
-//   }
-//
-//   // 클리어 캐쉬
-//   Future<void> clearOceanRaditionLevel() async{
-//         await openbox.clear();
-//   }
-//     // 검색
-//   Future<List<OceanRaditionLevelEntity>> searchOceanRaditionLevel(String query) async{
-//       final List<OceanRaditionLevelEntity> OceanRaditionLevelList = openbox.get('OceanRaditionLevelList', defaultValue: []);
-//       return OceanRaditionLevelList.where((e) =>
-//         e.gathMchnCd.contains(query) ||query == e.gathMchnCd ||  // 시료수거지원명 지역
-//         e.itmNm.contains(query) || query == e.itmNm ).toList(); // 품목명
-//   }
+
 
